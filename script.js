9a95b09ddf99962975e06e9aa80c3ab8c3dfb4c0
Your Name <jamalibrahim@jamals-mbp.attlocal.net>
2021-02-08 21:23:58 -0800

    second commit

--- script.js
+++ script.js
@@ -7,26 +7,26 @@ function runGame () {
 
 
     do {
-        guessString = prompt ('I am thinking of a number in the range 1 to 100. /n/nWhat is the number?');
+        guessString = prompt ('I am thinking of a number in the range 1 to 100. \n\nWhat is the number?');
         guessNumber = +guessString;
         numTries += 1;
 
         correct = checkGuess(guessNumber, target);
 
 
-    } while(!corrrect);
+    } while(!correct);
 
-    alert ('You got it! the Number was' + target + './n/nIt Took you ' + numTries + ' tries to guess Correctly!' );
+    alert ('You got it! the Number was' + target + '.\n\nIt Took you ' + numTries + ' tries to guess Correctly!' );
 }
 
 function checkGuess(guessNumber, target) {
     let correct = false;
 
     if (isNaN(guessNumber)) {
-        alert ( 'You have not entered a number. /n/nPlease enter a number in the 1-100 range');
+        alert ( 'You have not entered a number. \n\nPlease enter a number in the 1-100 range');
 
-    } else if ((guessNumber <1 || (guessNumber > 100) )) {
-        alert ('Please enter an integer in the 1-100 range.')
+    } else if ((guessNumber < 1 || (guessNumber > 100) )) {
+        alert ('Please enter an integer in the 1-100 range.');
     } else if (guessNumber > target) {
         alert('Your number is too Large');
     } else if (guessNumber < target) {
